31d9f77ce4fe24be6d2f3307d03b98336d7454bc
Mike Taylor <github@miket.dev>
2025-02-22 19:47:00 -0800

    add override of RSpec::Support::Differ#diff_as_string

---
+++ lib/rspec-difftastic.rb
@@ -0,0 +1,21 @@
+require 'difftastic'
+
+module RSpec
+  module Support
+    class Differ
+
+      DIFFTASTIC_DIFFER = Difftastic::Differ.new(
+        background: :dark,
+        color: :always,
+        left_label: "Expected",
+        right_label: "Actual"
+      )
+
+      def diff_as_string(actual, expected)
+        diff_string = DIFFTASTIC_DIFFER.diff_strings(actual, expected)
+        "\n\n#{diff_string}\n"
+      end
+
+    end
+  end
+end
